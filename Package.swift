d16af3beeef673c53dc8d64aa2350093c8f355a1
Wayne Westerman <wwesterman@apple.com>
2020-12-23 10:15:01 -0800

    Package Sources

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .target(
             name: "SortedSet",
-            path: "SortedSet"
+            path: "Sources"
         ),
     ]
     swiftLanguageVersions: [.v5]
